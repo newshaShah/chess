b0e19b0a280b37d3939b76360741acdaf563e3b0
9611046 <newsha.shah@gmail.com>
2019-05-16 21:44:31 +0430

    made Knight class

---
+++ src/pieces/Knight.java
@@ -0,0 +1,24 @@
+package pieces;
+
+import game.Square;
+
+import java.util.ArrayList;
+
+public class Knight extends Piece {
+    public Knight(String ID, String imagePath, String color) {
+        setPieceId(ID);
+        setImage(imagePath);
+        setPieceColor(color);
+    }
+    public ArrayList<Square> move(Square [][] boardSquares, int x, int y) {
+        possibleSquares.clear();
+        int posx[] = {x + 1, x + 1, x + 2, x + 2, x - 1, x - 1, x - 2, x - 2};
+        int posy[] = {y - 2, y + 2, y - 1, y + 1, y - 2, y + 2, y - 1, y + 1};
+        for (int i = 0; i < 8; i++)
+            if ((posx[i] >= 0 && posx[i] < 8 && posy[i] >= 0 && posy[i] < 8))
+                if ((boardSquares[posx[i]][posy[i]].getPiece() == null || boardSquares[posx[i]][posy[i]].getPiece().getPieceColor() != this.getPieceColor())) {
+                    possibleSquares.add(boardSquares[posx[i]][posy[i]]);
+                }
+    return possibleSquares;
+    }
+}
