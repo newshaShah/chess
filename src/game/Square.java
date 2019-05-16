f2b5d1a6717d9574a3c393e47e5cc309663fae29
9611046 <newsha.shah@gmail.com>
2019-05-16 16:20:40 +0430

    documentation for Square class

--- src/game/Square.java
+++ src/game/Square.java
@@ -18,6 +18,11 @@ public class Square extends JButton {
     private JLabel image;
     private boolean possibleSquare = false;
 
+    /**
+     *
+     * @param x x coordinate of square
+     * @param y y coordinate of square
+     */
     public Square(int x, int y){
         this.x=x;
         this.y = y;
@@ -25,6 +30,10 @@ public class Square extends JButton {
 
     }
 
+    /**
+     *
+     * @param piece put a piece on a chess square
+     */
     public void setPiece(Piece piece) {
         this.piece = piece;
         hasPiece = true;
@@ -33,32 +42,58 @@ public class Square extends JButton {
         this.add(image);
     }
 
+    /**
+     * selects a square by redrawing its line border
+     */
     public void selectSquare()
     {
         this.setBorder(BorderFactory.createLineBorder(Color.red,6));
         this.isSelected=true;
     }
+
+    /**
+     * deselects a square by deleting its red border line
+     */
     public void cancleSelection(){
-        this.isSelected=true;
+        this.isSelected=false;
         this.setBorder(null);
     }
 
+    /**
+     * makes a square possible for moving a piece by making its border line green
+     */
     public void makePossible(){
         this.setBorder(BorderFactory.createLineBorder(Color.green,6));
         this.possibleSquare = true;
     }
+
+    /**
+     * removes a piece of a square
+     */
     public void removePiece(){
         this.piece = null;
         this.remove(image);
     }
 
+    /**
+     *
+     * @return true if this square has a piece
+     */
     public boolean hasPiece() {
         return hasPiece;
     }
 
+    /**
+     * @return true if square is possible for moving a piece
+     */
     public boolean isPossibleSquare() {
         return possibleSquare;
     }
+
+    /**
+     *
+     * @return the piece of a square
+     */
     public Piece getPiece(){
         return piece;
     }
